282f7014ed3c8356c1da2fe82a15979420d384c0
SERRY SINGH <123100425+SerrySingh@users.noreply.github.com>
2023-12-12 23:40:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,8 @@
 # Create a new S3 bucket
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "serrylalotra"  #var.bucket_name  # Change this to your desired new bucket name
- 
+
+
 }
 
 output "bucket_name" {
