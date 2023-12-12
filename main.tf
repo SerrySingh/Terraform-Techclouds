c819b380a8fa965030f4985b35ce550aece84262
SERRY SINGH <123100425+SerrySingh@users.noreply.github.com>
2023-12-12 23:31:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,16 @@
 # Declare a variable for the S3 bucket name
-variable "bucket_name" {
-  description = "Enter a unique name for the new S3 bucket"
-}
+#variable "bucket_name" {
+ # description = "Enter a unique name for the new S3 bucket"
+#}
 # Declare a variable for the existing S3 bucket name
-variable "existing_bucket_name" {
-  description = "Enter the name of the existing S3 bucket"
-}
+#variable "existing_bucket_name" {
+#  description = "Enter the name of the existing S3 bucket"
+#}
 
 # Create a new S3 bucket
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = var.bucket_name  # Change this to your desired new bucket name
+  bucket = "serrylalotra"  #var.bucket_name  # Change this to your desired new bucket name
  
-# force_destroy = true
 }
 
 output "bucket_name" {
@@ -22,7 +21,7 @@ output "bucket_name" {
 resource "null_resource" "copy_to_s3" {
   provisioner "local-exec" {
     command = <<-EOT
-      aws s3 cp terraform.tfstate s3://${var.existing_bucket_name}/
+      aws s3 cp terraform.tfstate s3://yoyoserry/     #${var.existing_bucket_name}/
     EOT
   }
 }
